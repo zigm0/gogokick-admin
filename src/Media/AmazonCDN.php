fbb20f8e8d4ddc80289db10914914586e8001e0c
headzoo <sean@headzoo.io>
2019-08-08 16:18:40 -0400

    Adds CDNInterface

---
+++ src/Media/AmazonCDN.php
@@ -0,0 +1,180 @@
+<?php
+namespace App\Media;
+
+use Aws\CloudFront\CloudFrontClient;
+use Aws\S3\S3Client;
+use RuntimeException;
+
+/**
+ * Class AmazonCDN
+ */
+class AmazonCDN implements CDNInterface
+{
+    /**
+     * @var S3Client
+     */
+    protected $s3;
+
+    /**
+     * @var CloudFrontClient
+     */
+    protected $cloudFront;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @var MimeTypes
+     */
+    protected $mimeTypes;
+
+    /**
+     * Constructor
+     *
+     * @param S3Client  $s3
+     * @param CloudFrontClient $cloudFront
+     * @param array     $config
+     * @param MimeTypes $mimeTypes
+     */
+    public function __construct(S3Client $s3, CloudFrontClient $cloudFront, array $config, MimeTypes $mimeTypes)
+    {
+        $this->s3         = $s3;
+        $this->cloudFront = $cloudFront;
+        $this->config     = $config;
+        $this->mimeTypes  = $mimeTypes;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function upload($system, $path, $data, $progressFunc = null)
+    {
+        $config = $this->getSystemConfig($system);
+        $key    = $this->getSystemPath($system, $path);
+
+        $req = [
+            'Bucket'       => $config['s3']['bucket'],
+            'Key'          => $key,
+            'Body'         => $data,
+            'ContentType'  => $this->mimeTypes->getFromFilename($path),
+            'CacheControl' => $config['s3']['cacheControl'],
+            'ACL'          => 'public-read',
+        ];
+        if ($progressFunc) {
+            $req['@http'] = [
+                'progress' => $progressFunc
+            ];
+        }
+
+        $this->s3->putObject($req);
+
+        return $this->resolveUrl($system, $path);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function download($system, $path)
+    {
+        $config = $this->getSystemConfig($system);
+        $key    = $this->getSystemPath($system, $path);
+
+        $results = $this->s3->getObject([
+            'Bucket' => $config['s3']['bucket'],
+            'Key'    => $key
+        ]);
+
+        return (string)$results['Body'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function remove($system, $path)
+    {
+        $config = $this->getSystemConfig($system);
+        $key    = $this->getSystemPath($system, $path);
+
+        $this->s3->deleteObject([
+            'Bucket' => $config['s3']['bucket'],
+            'Key'    => $key,
+        ]);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function invalidate($system, $path)
+    {
+        $config = $this->getSystemConfig($system);
+        $key    = $this->getSystemPath($system, $path);
+
+        $this->cloudFront->createInvalidation([
+            'DistributionId' => $config['cloudfront']['id'],
+            'InvalidationBatch' => [
+                'CallerReference' => $key,
+                'Paths' => [
+                    'Items' => ['/' . $key],
+                    'Quantity' => 1,
+                ]
+            ]
+        ]);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function resolveUrl($system, $path, $verify = false)
+    {
+        $config = $this->getSystemConfig($system);
+        $key    = $this->getSystemPath($system, $path);
+
+        if ($verify) {
+            if (!$this->s3->doesObjectExist($config['s3']['bucket'], $key)) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Unable to resolve s3://%s/%s',
+                        $config['s3']['bucket'],
+                        $key
+                    )
+                );
+            }
+        }
+
+        return sprintf('https://%s/%s', $config['cloudfront']['url'], $key);
+    }
+
+    /**
+     * @param string $system
+     *
+     * @return array
+     */
+    protected function getSystemConfig($system)
+    {
+        if (!isset($this->config[$system])) {
+            throw new RuntimeException("Invalid CDN system ${system}.");
+        }
+
+        return $this->config[$system];
+    }
+
+    /**
+     * @param string $system
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function getSystemPath($system, $path)
+    {
+        $config = $this->getSystemConfig($system);
+        $path   = sprintf('%s/%s', trim($config['s3']['folder'], '/'), trim($path, '/'));
+
+        return $path;
+    }
+}
---
+++ src/Media/CDNInterface.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Media;
+
+use closure;
+
+/**
+ * Interface CDNInterface
+ */
+interface CDNInterface
+{
+    /**
+     * Uploads the data to the CDN and returns the URL
+     *
+     * @param string $system
+     * @param string $path
+     * @param string $data
+     * @param closure $progressFunc ($downloadTotalSize, $downloadSizeSoFar, $uploadTotalSize, $uploadSizeSoFar)
+     *
+     * @return string
+     */
+    public function upload($system, $path, $data, $progressFunc = null);
+
+    /**
+     * Returns the contents of the file at the given path
+     *
+     * @param string $system
+     * @param string $path
+     *
+     * @return string
+     */
+    public function download($system, $path);
+
+    /**
+     * Deletes the file at the given path
+     *
+     * @param string $system
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function remove($system, $path);
+
+    /**
+     * Invalidate the cache for the given path
+     *
+     * @param string $system
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function invalidate($system, $path);
+
+    /**
+     * Returns the URL for the given path
+     *
+     * When $verify is true the method throws an exception if the file
+     * does not exist in S3.
+     *
+     * @param string $system
+     * @param string $path
+     * @param bool   $verify
+     *
+     * @return string
+     */
+    public function resolveUrl($system, $path, $verify = false);
+}
