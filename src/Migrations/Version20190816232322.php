754a46d04daa815a5af15d57048ace37761777c9
headzoo <sean@headzoo.io>
2019-08-16 19:48:56 -0400

    Adds migration

---
+++ src/Migrations/Version20190816232322.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190816232322 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE block ADD text LONGTEXT NOT NULL, ADD image VARCHAR(255) DEFAULT NULL, ADD video VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE project ADD campaign_type SMALLINT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE block DROP text, DROP image, DROP video');
+        $this->addSql('ALTER TABLE project DROP campaign_type');
+    }
+}
