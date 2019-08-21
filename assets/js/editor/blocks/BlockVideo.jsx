02ccac4cdf08f6b681ca2da4943fbed22456fae9
headzoo <sean@headzoo.io>
2019-08-20 20:28:34 -0400

    Updates favicons

--- assets/js/editor/blocks/BlockVideo.jsx
+++ assets/js/editor/blocks/BlockVideo.jsx
@@ -22,7 +22,8 @@ export default class BlockVideo extends React.PureComponent {
     const opts = {
       width:      '100%',
       playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 0
+        autoplay: 0,
+        rel:      0
       }
     };
 
