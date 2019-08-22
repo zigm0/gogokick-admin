10cccfd4fdcd90998f9c8aeab5a673ca3ccb563b
headzoo <sean@headzoo.io>
2019-08-21 21:45:27 -0400

    Tweak

--- assets/js/utils/react.js
+++ assets/js/utils/react.js
@@ -15,7 +15,7 @@ export function reactDisplayName(WrappedComponent) {
 export function reactTraverseChildren(children, cb) {
   React.Children.map(children, (child) => {
     cb(child);
-    if (child.props && child.props.children) {
+    if (child && child.props && child.props.children) {
       reactTraverseChildren(child.props.children, cb);
     }
   });
