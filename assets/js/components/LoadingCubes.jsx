3dd5c08e28f85b29d414ccfd84dcbcbbdc092f56
headzoo <sean@headzoo.io>
2019-08-26 07:34:36 -0400

    Updates LoadingCubes

--- assets/js/components/LoadingCubes.jsx
+++ assets/js/components/LoadingCubes.jsx
@@ -23,7 +23,12 @@ export default class LoadingCubes extends React.PureComponent {
 
     return (
       <div className={classes}>
-        <div className="loading-cube-inner" style={{ width: '100%', height: '100%' }}>
+        <div className="lds-grid" style={{ width: '100%', height: '100%' }}>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
           <div />
           <div />
           <div />
