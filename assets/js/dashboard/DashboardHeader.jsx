ff791cd7891b98e826ad31105bbf94c26b392406
headzoo <sean@headzoo.io>
2019-09-22 19:59:49 -0400

    Updates DashboardHeader

--- assets/js/dashboard/DashboardHeader.jsx
+++ assets/js/dashboard/DashboardHeader.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect, mapDispatchToProps } from 'utils';
 import { Link } from 'components';
-import { Container } from 'components/bootstrap';
 import UserMenu from 'layout/UserMenu';
 import { editorActions, projectActions, uiActions } from 'actions';
 
@@ -29,34 +28,32 @@ export default class DashboardHeader extends React.PureComponent {
 
     return (
       <header className="editor-header dashboard-header">
-        <Container className="d-flex align-items-center">
-          <div className="editor-header-left">
-            <Link to="/">
-              <img src="/images/logo-2.png" alt="Logo" className="logo-desktop" />
-              <img src="/images/logo-square.png" alt="Logo" className="logo-mobile" />
+        <div className="editor-header-left">
+          <Link to="/">
+            <img src="/images/logo-2.png" alt="Logo" className="logo-desktop" />
+            <img src="/images/logo-square.png" alt="Logo" className="logo-mobile" />
+          </Link>
+        </div>
+        <div className="editor-header-middle home-page-header-links">
+          <Link to="/editor/new">
+            Start a project
+          </Link>
+          {isAuthenticated ? (
+            <Link to="/dashboard">
+              Dashboard
             </Link>
-          </div>
-          <div className="editor-header-middle home-page-header-links">
-            <Link to="/editor/new">
-              Start a project
+          ) : (
+            <Link to="/login">
+              Login
             </Link>
-            {isAuthenticated ? (
-              <Link to="/dashboard">
-                Dashboard
-              </Link>
-            ) : (
-              <Link to="/login">
-                Login
-              </Link>
-            )}
-            <Link to="/help">
-              Help
-            </Link>
-          </div>
-          <div className="editor-header-right">
-            <UserMenu />
-          </div>
-        </Container>
+          )}
+          <Link to="/help">
+            Help
+          </Link>
+        </div>
+        <div className="editor-header-right">
+          <UserMenu />
+        </div>
       </header>
     );
   }
