43dc5dbb780c4362d30da664969c73a3b2e4a235
headzoo <sean@headzoo.io>
2019-10-28 13:15:36 -0400

    Updates deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh ubuntu@18.208.184.10 'cd /var/www/gogokick.com/ && git fetch --all && git reset --hard origin/master && bin/console assets:install --symlink && yarn run build && bin/console cache:clear'
+ssh ubuntu@3.233.21.193 'cd /var/www/admin/ && git fetch --all && git reset --hard origin/master && bin/console assets:install --symlink && yarn run build && bin/console cache:clear'
