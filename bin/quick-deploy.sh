f204d1c8827d91fe0babb289ff01da9f414da04c
headzoo <sean@headzoo.io>
2019-10-30 12:17:45 -0400

    Adds quick-deploy.sh

---
+++ bin/quick-deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh ubuntu@18.208.184.10 'cd /var/www/gogokick.com/ && git fetch --all && git reset --hard origin/master && bin/console assets:install --symlink && bin/console cache:clear'
