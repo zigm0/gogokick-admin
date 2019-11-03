6cbe305a09255e54619916c09dd412a0029a60df
headzoo <sean@headzoo.io>
2019-11-03 10:46:11 -0500

    Formatting

--- assets/js/store/history.js
+++ assets/js/store/history.js
@@ -2,9 +2,6 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 history.listen((location) => {
-/*  window.ga('set', 'page', location.pathname + location.search);
-  window.ga('send', 'pageview');*/
-
   window.gtag('config', 'UA-151267728-1', {
     'page_title':    document.title,
     'page_location': window.location.href,
